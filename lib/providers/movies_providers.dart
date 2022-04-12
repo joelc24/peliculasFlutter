577ad97042b57fb0154dea7eb1780f9586e4b156
Joel <libra-marriaga@hotmail.com>
2022-04-12 14:31:35 -0500

    Mapear una peticion http

--- lib/providers/movies_providers.dart
+++ lib/providers/movies_providers.dart
@@ -1,9 +1,17 @@
 
-
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
+import '../models/models.dart';
+
 class MoviesProvider extends ChangeNotifier {
+
+  String _apiKey = '587601a37c6d1275f62efb95b0d9b501';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-ES';
 
   MoviesProvider(){
     print("MoviesProvider Inicializado");
@@ -13,7 +21,17 @@ class MoviesProvider extends ChangeNotifier {
 
 
   getOnDisplayMovies() async {
-    print("MoviesProvider getOnDisplayMovies");
+     var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+       
+      });
+
+      final response = await http.get(url);
+      final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+      print(nowPlayingResponse.results[0].title);
   }
 
 }
