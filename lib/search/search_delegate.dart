75f3bf6146085886a4c5e51b14f7261eb9b97712
Joel <libra-marriaga@hotmail.com>
2022-04-14 17:02:00 -0500

    Mostrar las peliculas basadas en el query

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,6 +1,10 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/models.dart';
+import '../providers/movies_providers.dart';
 
 class MovieSearchDelegate extends SearchDelegate{
 
@@ -31,19 +35,63 @@ class MovieSearchDelegate extends SearchDelegate{
     return const Text('Resultados');
   }
 
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    if(query.isEmpty) {
-      return Container(
+  Widget _EmptyContainer(){
+    return Container(
         child: const Center(
           child: Icon(Icons.movie_creation_outlined, color: Colors.black38, size: 130,),
         ),
-      );
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if(query.isEmpty) {
+      return _EmptyContainer();
     }
 
-    return Container();
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
+    return FutureBuilder(
+      future: moviesProvider.searchMovie(query),
+      builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+
+        if(!snapshot.hasData) return _EmptyContainer();
+
+        final movies = snapshot.data!;
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (_, int index) => _MovieItem(movie: movies[index]),
+
+        );
+      },
+    );
   }
 
 
 
 }
+
+class _MovieItem extends StatelessWidget {
+
+  final Movie movie;
+
+  const _MovieItem({ Key? key, required this.movie }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        placeholder: const AssetImage('assets/no-image.jpg'), 
+        image: NetworkImage(movie.getPosterPath),
+        width: 50,
+        fit: BoxFit.contain
+      ),
+      title: Text(movie.title),
+      subtitle: Text(movie.releaseDate ?? ''),
+      onTap: () => Navigator.pushNamed(context, 'details', arguments: movie),
+    );
+  }
+}
+
